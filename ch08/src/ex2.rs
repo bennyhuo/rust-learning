982c4cb0d6182c64c9cb5ba30e4ee129201f3cab
bennyhuo <bennyhuo@kotliner.cn>
2023-04-27 23:23:10 +0800

    update

--- ch08/src/ex2.rs
+++ ch08/src/ex2.rs
@@ -1,29 +1,9 @@
 pub fn main() {
-    println!("pig latin: {}", to_pig_latin2(&"Hello World!".to_string()));
-    println!("pig latin: {}", to_pig_latin2(&"".to_string()));
+    println!("pig latin: {}", to_pig_latin(&"Hello World!".to_string()));
+    println!("pig latin: {}", to_pig_latin(&"".to_string()));
 }
 
 fn to_pig_latin(string: &String) -> String {
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-    let mut pig_latin = String::new();
-
-    for word in string.split_whitespace() {
-        let mut chars = word.chars();
-        let first = chars.next().unwrap();
-
-        let new_word = if vowels.contains(&first) {
-            format!("{word}-hay ")
-        } else {
-            format!("{}-{first}ay ", chars.as_str())
-        };
-        pig_latin.push_str(new_word.as_str());
-    }
-
-    pig_latin.pop();
-    pig_latin
-}
-
-fn to_pig_latin2(string: &String) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
 
     let mut pig_latin = string
@@ -31,12 +11,12 @@ fn to_pig_latin2(string: &String) -> String {
         .fold(String::new(), |mut acc, word| {
             let mut chars = word.chars();
             let first = chars.next().unwrap();
-            let new_word = if vowels.contains(&first) {
+            let new_word = if vowels.contains(&first.to_ascii_lowercase()) {
                 format!("{word}-hay ")
             } else {
                 format!("{}-{first}ay ", chars.as_str())
             };
-            acc.push_str(new_word.as_str());
+            acc.push_str(&new_word);
             acc
         });
 
