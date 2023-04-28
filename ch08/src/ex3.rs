459031efdf56700078e83fdff749a7dbd4e0163e
bennyhuo <bennyhuo@kotliner.cn>
2023-04-28 08:29:16 +0800

    update

--- ch08/src/ex3.rs
+++ ch08/src/ex3.rs
@@ -2,14 +2,20 @@ use std::collections::HashMap;
 
 pub fn main() {
     let mut company = Company::new();
-    company.add_employee("Benny", "Kotlin UG");
-    company.add_employee("Benny", "Kotlin UG");
-    company.add_employee("Benny", "Kotlin UG");
-    company.add_employee("Benny", "Kotlin UG");
+    company.add_employee("E", "Kotlin");
+    company.add_employee("B", "Kotlin");
+    company.add_employee("C", "Kotlin");
+    company.add_employee("A", "Kotlin1");
+    company.add_employee("D", "Kotlin1");
 
-    company
-        .list_employees("Kotlin UG")
-        .map(|employees| employees.iter().for_each(|e| println!("{e}")));
+    company.list_employees("Kotlin").iter().for_each(|e| {
+        println!("{e}");
+    });
+
+    println!("----");
+    company.list_all_employees().iter().for_each(|e| {
+        println!("{e}");
+    })
 }
 
 struct Company {
@@ -30,7 +36,24 @@ impl Company {
             .push(name.to_string())
     }
 
-    fn list_employees(&self, department: &str) -> Option<&Vec<String>> {
-        self.departments.get(department)
+    fn list_employees(&self, department: &str) -> Vec<String> {
+        self.departments
+            .get(department)
+            .map(|v| {
+                let mut result = v.to_vec();
+                result.sort();
+                result
+            })
+            .unwrap_or(Vec::new())
+    }
+
+    fn list_all_employees(&self) -> Vec<String> {
+        let mut result = self.departments.iter().fold(Vec::new(), |mut acc, e| {
+            acc.append(&mut e.1.to_vec());
+            acc
+        });
+
+        result.sort();
+        result
     }
 }
